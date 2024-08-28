d1c6a8213edf9244ba5c14b66d36f38e1db301e2
DimitriChrysafis <Dimitri.chrysafis@gmail.com>
2024-08-27 18:14:51 -0700

    changed config files
    changed config files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/dimitrichrysafis/CLionProjects/mvc
+# Install script for directory: /Users/dimitrichrysafis/Documents/GitHub/3d-Grapher
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/dimitrichrysafis/CLionProjects/mvc/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/dimitrichrysafis/Documents/GitHub/3d-Grapher/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
